b34dc369c8e4383dda5185d701c17fceaec4ddff
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-01 15:56:42 +0200

    Update SPI_Config.c

--- SPI_Config.c
+++ SPI_Config.c
@@ -3,21 +3,21 @@
 void SPI_Init( Struct_SPI SPI_Config){
 	switch(SPI_Config.Operation_Mode){
 		case Master : SPCR |= (1U << MSTR);
-					  (*((volatile u8*)(SCL_PIN  + 1))) |= (1U << SCL_Bit);	/*	At MASTER SCL Output		*/
-					  (*((volatile u8*)(MOSI_PIN + 1))) |= (1U << MOSI_Bit);/*	At MASTER MOSI as Output	*/
-					  break;				
+			    (*((volatile u8*)(SCL_PIN  + 1))) |= (1U << SCL_Bit);/*	At MASTER SCL Output		*/
+			    (*((volatile u8*)(MOSI_PIN + 1))) |= (1U << MOSI_Bit);/*	At MASTER MOSI as Output	*/
+			    break;				
 					  	  
 		case Slave  : SPCR &=~(1U << MSTR);
-					  (*((volatile u8*)(MISO_PIN + 1))) |= (1 << MISO_Bit);/*	At SLAVE MISO as Output		*/
-					  break;
+			    (*((volatile u8*)(MISO_PIN + 1))) |= (1 << MISO_Bit);/*	At SLAVE MISO as Output		*/
+			    break;
 					  
-		default		: SPCR &=~(1U << MSTR);
-					  (*((volatile u8*)(MISO_PIN + 1))) |= (1 << MISO_Bit);/*	At SLAVE MISO as Output		*/
-					  break;
+		default	    : SPCR &=~(1U << MSTR);
+			    (*((volatile u8*)(MISO_PIN + 1))) |= (1 << MISO_Bit);/*	At SLAVE MISO as Output		*/
+			    break;
 	}
 	
 	switch(SPI_Config.SCK_Frequency){
-		case Fosc_4			: SPSR &=~(1U << SPI2X); SPCR &=~(1U << SPR1); SPCR &=~(1U << SPR0); break;
+		case Fosc_4		: SPSR &=~(1U << SPI2X); SPCR &=~(1U << SPR1); SPCR &=~(1U << SPR0); break;
 		case Fosc_16		: SPSR &=~(1U << SPI2X); SPCR &=~(1U << SPR1); SPCR |= (1U << SPR0); break;
 		case Fosc_64		: SPSR &=~(1U << SPI2X); SPCR |= (1U << SPR1); SPCR &=~(1U << SPR0); break;
 		case Fosc_128		: SPSR &=~(1U << SPI2X); SPCR |= (1U << SPR1); SPCR |= (1U << SPR0); break;
@@ -25,7 +25,7 @@ void SPI_Init( Struct_SPI SPI_Config){
 		case Fosc_2x_8		: SPSR |= (1U << SPI2X); SPCR &=~(1U << SPR1); SPCR |= (1U << SPR0); break;
 		case Fosc_2x_32		: SPSR |= (1U << SPI2X); SPCR |= (1U << SPR1); SPCR &=~(1U << SPR0); break;
 		case Fosc_2x_64		: SPSR |= (1U << SPI2X); SPCR |= (1U << SPR1); SPCR |= (1U << SPR0); break;
-		default				: SPSR &=~(1U << SPI2X); SPCR &=~(1U << SPR1); SPCR &=~(1U << SPR0); break;
+		default			: SPSR &=~(1U << SPI2X); SPCR &=~(1U << SPR1); SPCR &=~(1U << SPR0); break;
 	}
 	
 	switch(SPI_Config.Data_Order){
@@ -39,12 +39,12 @@ void SPI_Init( Struct_SPI SPI_Config){
 		case Leading_Edge_Sample_Falling : SPCR |= (1U << CPOL); SPCR &=~(1U << CPHA); break;
 		case Leading_Edge_Setup_Rising	 : SPCR &=~(1U << CPOL); SPCR |= (1U << CPHA); break;
 		case Leading_Edge_Setup_Falling	 : SPCR |= (1U << CPOL); SPCR |= (1U << CPHA); break;
-		default							 : SPCR &=~(1U << CPOL); SPCR &=~(1U << CPHA); break;
+		default				 : SPCR &=~(1U << CPOL); SPCR &=~(1U << CPHA); break;
 	}
 	
 	switch(SPI_Config.Interrupt_Select){
 		case Interrupt_Enable	: SPCR |= (1U << SPIE); break;
-		default					: SPCR &=~(1U << SPIE); break;
+		default			: SPCR &=~(1U << SPIE); break;
 	}
 	
 	SPCR |= (1 << SPE);		/*	After Finishing the configuration, Enable the SPI	*/
@@ -52,9 +52,9 @@ void SPI_Init( Struct_SPI SPI_Config){
 }
 
 u8 SPI_Transever ( u8 Data ){
-	SPDR = Data;					/*	Send Data to the Data register		*/
+	SPDR = Data;			/*	Send Data to the Data register		*/
 	while (!(SPSR & (1 << SPIF)));	/*	Wait until transmission is complete	
-										After Transmitting the data it will	
-										Be replaced by the received Data	*/
-	return SPDR;					/*	Return the Received Data from SPDR	*/
+						After Transmitting the data it will	
+						Be replaced by the received Data	*/
+	return SPDR;			/*	Return the Received Data from SPDR	*/
 }
